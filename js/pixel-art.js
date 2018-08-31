b91fddd25239bea21d3f55e54099e28919ce8543
John-Debon <matudbonis@gmail.com>
2018-08-31 14:18:44 -0300

    Colors added. Palette added, click painting added

--- js/pixel-art.js
+++ js/pixel-art.js
@@ -1,35 +1,174 @@
-var nombreColores = ['White', 'LightYellow',
-  'LemonChiffon', 'LightGoldenrodYellow', 'PapayaWhip', 'Moccasin', 'PeachPuff', 'PaleGoldenrod', 'Bisque', 'NavajoWhite', 'Wheat', 'BurlyWood', 'Tan',
-  'Khaki', 'Yellow', 'Gold', 'Orange', 'DarkOrange', 'OrangeRed', 'Tomato', 'Coral', 'DarkSalmon', 'LightSalmon', 'LightCoral', 'Salmon', 'PaleVioletRed',
-  'Pink', 'LightPink', 'HotPink', 'DeepPink', 'MediumVioletRed', 'Crimson', 'Red', 'FireBrick', 'DarkRed', 'Maroon',
-  'Brown', 'Sienna', 'SaddleBrown', 'IndianRed', 'RosyBrown',
-  'SandyBrown', 'Goldenrod', 'DarkGoldenrod', 'Peru',
-  'Chocolate', 'DarkKhaki', 'DarkSeaGreen', 'MediumAquaMarine',
-  'MediumSeaGreen', 'SeaGreen', 'ForestGreen', 'Green', 'DarkGreen', 'OliveDrab', 'Olive', 'DarkOliveGreen', 'YellowGreen', 'LawnGreen',
-  'Chartreuse', 'GreenYellow', 'Lime', 'SpringGreen', 'LimeGreen',
-  'LightGreen', 'PaleGreen', 'PaleTurquoise',
-  'AquaMarine', 'Cyan', 'Turquoise', 'MediumTurquoise', 'DarkTurquoise', 'DeepSkyBlue',
-  'LightSeaGreen', 'CadetBlue', 'DarkCyan', 'Teal', 'Steelblue', 'LightSteelBlue', 'Honeydew', 'LightCyan',
-  'PowderBlue', 'LightBlue', 'SkyBlue', 'LightSkyBlue',
-  'DodgerBlue', 'CornflowerBlue', 'RoyalBlue', 'SlateBlue',
-  'MediumSlateBlue', 'DarkSlateBlue', 'Indigo', 'Purple', 'DarkMagenta', 'Blue',
-  'MediumBlue', 'DarkBlue', 'Navy', 'Thistle',
-  'Plum', 'Violet', 'Orchid', 'DarkOrchid', 'Fuchsia', 'Magenta', 'MediumOrchid',
-  'BlueViolet', 'DarkViolet', 'DarkOrchid',
-  'MediumPurple', 'Lavender', 'Gainsboro', 'LightGray', 'Silver', 'DarkGray', 'Gray',
-  'DimGray', 'LightSlateGray', 'DarkSlateGray', 'Black'
+var nombreColores = [
+    'White',
+    'LightYellow',
+    'LemonChiffon',
+    'LightGoldenrodYellow',
+    'PapayaWhip',
+    'Moccasin',
+    'PeachPuff',
+    'PaleGoldenrod',
+    'Bisque',
+    'NavajoWhite',
+    'Wheat',
+    'BurlyWood',
+    'Tan',
+    'Khaki',
+    'Yellow',
+    'Gold',
+    'Orange',
+    'DarkOrange',
+    'OrangeRed',
+    'Tomato',
+    'Coral',
+    'DarkSalmon',
+    'LightSalmon',
+    'LightCoral',
+    'Salmon',
+    'PaleVioletRed',
+    'Pink',
+    'LightPink',
+    'HotPink',
+    'DeepPink',
+    'MediumVioletRed',
+    'Crimson',
+    'Red',
+    'FireBrick',
+    'DarkRed',
+    'Maroon',
+    'Brown',
+    'Sienna',
+    'SaddleBrown',
+    'IndianRed',
+    'RosyBrown',
+    'SandyBrown',
+    'Goldenrod',
+    'DarkGoldenrod',
+    'Peru',
+    'Chocolate',
+    'DarkKhaki',
+    'DarkSeaGreen',
+    'MediumAquaMarine',
+    'MediumSeaGreen',
+    'SeaGreen',
+    'ForestGreen',
+    'Green',
+    'DarkGreen',
+    'OliveDrab',
+    'Olive',
+    'DarkOliveGreen',
+    'YellowGreen',
+    'LawnGreen',
+    'Chartreuse',
+    'GreenYellow',
+    'Lime',
+    'SpringGreen',
+    'LimeGreen',
+    'LightGreen',
+    'PaleGreen',
+    'PaleTurquoise',
+    'AquaMarine',
+    'Cyan',
+    'Turquoise',
+    'MediumTurquoise',
+    'DarkTurquoise',
+    'DeepSkyBlue',
+    'LightSeaGreen',
+    'CadetBlue',
+    'DarkCyan',
+    'Teal',
+    'Steelblue',
+    'LightSteelBlue',
+    'Honeydew',
+    'LightCyan',
+    'PowderBlue',
+    'LightBlue',
+    'SkyBlue',
+    'LightSkyBlue',
+    'DodgerBlue',
+    'CornflowerBlue',
+    'RoyalBlue',
+    'SlateBlue',
+    'MediumSlateBlue',
+    'DarkSlateBlue',
+    'Indigo',
+    'Purple',
+    'DarkMagenta',
+    'Blue',
+    'MediumBlue',
+    'DarkBlue',
+    'Navy',
+    'Thistle',
+    'Plum',
+    'Violet',
+    'Orchid',
+    'DarkOrchid',
+    'Fuchsia',
+    'Magenta',
+    'MediumOrchid',
+    'BlueViolet',
+    'DarkViolet',
+    'DarkOrchid',
+    'MediumPurple',
+    'Lavender',
+    'Gainsboro',
+    'LightGray',
+    'Silver',
+    'DarkGray',
+    'Gray',
+    'DimGray',
+    'LightSlateGray',
+    'DarkSlateGray',
+    'Black'
 ];
-
-// Variable para guardar el elemento 'color-personalizado'
-// Es decir, el que se elige con la rueda de color.
+var paleta = $('#paleta')[0];
+var grillaPixeles = $('#grilla-pixeles')[0];
+// Variable para guardar el elemento 'color-personalizado, Es decir, el que se elige con la rueda de color.
 var colorPersonalizado = document.getElementById('color-personalizado');
 
-colorPersonalizado.addEventListener('change', 
-  (function() {
+$('document').ready(init);
+
+function init() {
+    generarPaleta();
+    generarGrilla();
+    detectarcCambioColor();
+    detectarPintarPixel();
+}
+
+colorPersonalizado.addEventListener('change', function() {
     // Se guarda el color de la rueda en colorActual
     colorActual = colorPersonalizado.value;
     // Completar para que cambie el indicador-de-color al colorActual
+});
 
+function generarPaleta() {
+    for (var i = 0; i < nombreColores.length; i++) {
+        var color = nombreColores[i];
+        var casillaPaleta = $('<div>').appendTo('#paleta');
+        casillaPaleta.css('background-color', color);
+        casillaPaleta.addClass('color-paleta');
+    }
+}
 
-  })
-);
+function generarGrilla() {
+    for (var i = 0; i < 1750; i++) {
+        var pixel = $('<div>').appendTo('#grilla-pixeles');
+    }
+}
+
+function detectarcCambioColor() {
+    $('#paleta div').on('click', cambiarColor);
+}
+
+function cambiarColor() {
+    var colorAhora = $(this).css('background-color');
+    $('#indicador-de-color').css('background-color', colorAhora);
+}
+
+function detectarPintarPixel() {
+    $('#grilla-pixeles div').on('click', PintarPixel);
+}
+
+function PintarPixel() {
+    var colorAhora2 = $('#indicador-de-color').css('background-color');
+    $(this).css('background-color', colorAhora2);
+}
